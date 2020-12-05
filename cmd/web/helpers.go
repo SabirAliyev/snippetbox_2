cc4cb629add3d0a1962cf54b57da0d8012e11e97
Sabir Aliyev <sabir.aliyev.itdep@gmail.com>
2020-12-05 11:32:24 +0400

    Common Dynamic Data (147 - 149)

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 // The ServerError helper writes an error message and stack trace to the errorLog,
@@ -32,7 +33,18 @@ func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
-func (app *application) render(w http.ResponseWriter, _ *http.Request, name string, td *templateData) {
+// Create an addDefaultData helper. This takes a pointer to a TemplateData struct, add the current year
+// to the CurrentYear field, and then returns the pointer. Again, we`re not using the *http.Request
+// parameter at the moment.
+func (app *application) addDefaultData(td *templateData, _ *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
 	// Retrieve the appropriate template set from the cache based on the page name
 	// (like 'home.page.tmpl'). If no entry exists in the cache with the provided name,
 	// call the serverError helper that we made earlier.
@@ -45,9 +57,10 @@ func (app *application) render(w http.ResponseWriter, _ *http.Request, name stri
 	// Initialize a new buffer.
 	buf := new(bytes.Buffer)
 
-	// Write the template to the buffer, instead of straight to the http.ResponseWriter.
+	// Write the template to the buffer (buf), instead of straight to the http.ResponseWriter.
 	// If there is an error, call our serverError helper and then return.
-	err := ts.Execute(buf, td)
+	// Execute the template set, passing the dynamic data with current year injected (app.addDefaultData())
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
